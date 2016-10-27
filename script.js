45507571523e17bfe821a4b56dfec3ed196422f0
Dimas <dimsrad307@gmail.com>
2016-10-27 09:25:23 +0700

    Added bg color change on click

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?';
+var counter = 0;
+var colors = [    
+    'lightcoral',
+    'lightcyan',
+    'lightgreen',
+    'lightgoldenrodyellow',
+    'lightgray',
+    'lightpink',
+    'lightblue',
+]
 
 $(document).ready(function(){
     $.getJSON(url, function(data){
@@ -11,7 +21,8 @@ $(document).ready(function(){
                     $('cite').html('-' +author);                                                  
             });
 
-    $('#new').on('click', function(){                                
+    $('#new').on('click', function(){
+        counter++;                              
             $.getJSON(url, function(data){
                 var author = data.quoteAuthor;
                 
@@ -21,6 +32,15 @@ $(document).ready(function(){
                     } 
                     $('cite').html('-' +author); 
                                                              
+            });
+
+            $('.well').css({
+                'background-color': colors[(counter)%colors.length],
+                'border-color': colors[(counter)%colors.length]              
+            });
+
+            $('a,a:link,a:visited').css({
+                'color': colors[(counter)%colors.length]
             });
             
     });
