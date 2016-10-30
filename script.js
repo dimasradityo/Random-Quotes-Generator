64fae33a35450f9046fac070d8f6021324b0f31b
Dimas <dimsrad307@gmail.com>
2016-10-30 19:14:20 +0700

    Finished script and project

--- script.js
+++ script.js
@@ -9,39 +9,61 @@ var colors = [
     'lightpink',
     'lightblue',
 ]
+var author = '';
+var quote = '';
+var encode = '';
+var twit = 'https://twitter.com/intent/tweet?text=';
+
 
 $(document).ready(function(){
     $.getJSON(url, function(data){
-        var author = data.quoteAuthor;
+         author = data.quoteAuthor;
+         quote = '"' +data.quoteText+ '"';         
                 
-                    $('blockquote').html('"' +data.quoteText+ '"');
+                    $('blockquote').hide().html(quote).fadeIn(2000);
                     if (author == ''){
                         author = 'Unknown';
                     }
-                    $('cite').html('-' +author);                                                  
+                    author = '-' + author;
+                    $('cite').hide().html(author).fadeIn(4000);
+
+        encode = encodeURIComponent(quote + ' ' +author);               
             });
+
+    $('#tweet').on('click', function(){
+        $('#tweet').attr('href', twit.concat(encode));
+        window.open(this.href, 'newTweet','left=300,top=350,width=700,height=270,toolbar=1,resizable=0'); return false;
+    });                            
 
     $('#new').on('click', function(){
         counter++;                              
             $.getJSON(url, function(data){
-                var author = data.quoteAuthor;
-                
-                    $('blockquote').html(data.quoteText);
+                 author = data.quoteAuthor;
+                 quote = '"' +data.quoteText+ '"';                                
+                    $('blockquote').hide().fadeOut(1000).html(quote).fadeIn(2000);
+                    
                     if (author == ''){
                         author = 'Unknown';
                     } 
-                    $('cite').html('-' +author); 
+                    author = '-' + author;
+                    $('cite').hide().fadeOut(1000).html(author).fadeIn(4000); 
+
+                    encode = encodeURIComponent(quote + ' ' +author);
+
+                    $('.well').css({
+                        transition: 'background-color 1s ease-in-out',
+                        'background-color': colors[(counter)%colors.length],                        
+                        'border-color': colors[(counter)%colors.length]
+                    });
+
+                    $('a,a:link,a:visited').css({
+                        transition: 'color 1s ease-in-out',
+                        'color': colors[(counter)%colors.length]
+                    });
                                                              
             });
 
-            $('.well').css({
-                'background-color': colors[(counter)%colors.length],
-                'border-color': colors[(counter)%colors.length]              
-            });
-
-            $('a,a:link,a:visited').css({
-                'color': colors[(counter)%colors.length]
-            });
+            
             
     });
 });
